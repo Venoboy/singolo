6342bab8171f1ba6e8b043d53cfeb955017fc102
Admin <d>
2020-03-16 13:54:09 +0300

    fix: some bugs

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const menu = document.getElementById('menu');
+
+let phoneVertical = document.querySelector(".first-pic-click"),
+  phoneHorizontal = document.querySelector(".second-pic-click"),
+  screenVertical = document.getElementById("screen-vertical"),
+  screenHorizontal = document.getElementById("screen-horizontal");
+
+//-------------------Переменные-----------------------------
+
 menu.addEventListener('click', (event) => {
   menu.querySelectorAll('li').forEach(node => {
     node.firstChild.classList.remove('mark')
@@ -14,14 +22,15 @@ const arrowRight = document.getElementById('arrow-right'),
   sliderBackground = document.querySelector(".slider");
 
 
-const rightHandler = (event) => {
+const rightHandler = () => {
   arrowRight.removeEventListener("click", rightHandler);
   const firstPicture = pictureList.querySelectorAll('.slider-pic-container')[0];
-  const secondPicture = pictureList.querySelectorAll('.slider-pic-container')[1];
 
   if (firstPicture.id === 'first-pic') {
     sliderBackground.style.background = "#648BF0"
-  } else sliderBackground.style.background = "#F06C64";
+  } else {
+    sliderBackground.style.background = "#F06C64";
+  }
   firstPicture.style.marginLeft = "-100%";
   const firstPictureClone = firstPicture.cloneNode(true);
   firstPictureClone.style.marginLeft = "0";
@@ -31,10 +40,17 @@ const rightHandler = (event) => {
   });
   pictureList.append(firstPictureClone);
 
+  phoneVertical = document.querySelector(".first-pic-click");
+  phoneHorizontal = document.querySelector(".second-pic-click");
+  screenVertical = document.getElementById("screen-vertical");
+  screenHorizontal = document.getElementById("screen-horizontal");
+
+  phoneVertical.addEventListener("click", clickVerticalHandler);
+  phoneHorizontal.addEventListener("click", clickHorizontalHandler);
 };
 
 
-const leftHandler = (event) => {
+const leftHandler = () => {
   arrowLeft.removeEventListener("click", leftHandler);
   const firstPicture = pictureList.querySelectorAll('.slider-pic-container')[0];
   const secondPicture = pictureList.querySelectorAll('.slider-pic-container')[1];
@@ -42,16 +58,26 @@ const leftHandler = (event) => {
   const secondPictureClone = secondPicture.cloneNode(true);
   secondPictureClone.style.marginLeft = "-100%";
   pictureList.prepend(secondPictureClone);
+  phoneVertical = document.querySelector(".first-pic-click");
+  phoneHorizontal = document.querySelector(".second-pic-click");
+  screenVertical = document.getElementById("screen-vertical");
+  screenHorizontal = document.getElementById("screen-horizontal");
+
+  phoneVertical.addEventListener("click", clickVerticalHandler);
+  phoneHorizontal.addEventListener("click", clickHorizontalHandler);
+
 
   setTimeout(() => {
-    if (firstPicture.id === 'first-pic') {
-      sliderBackground.style.background = "#648BF0"
-    } else sliderBackground.style.background = "#F06C64";
     secondPictureClone.addEventListener("transitionend", () => {
       secondPicture.remove();
       arrowLeft.addEventListener("click", leftHandler)
     });
     secondPictureClone.style.marginLeft = "0";
+    if (firstPicture.id === 'first-pic') {
+      sliderBackground.style.background = "#648BF0"
+    } else {
+      sliderBackground.style.background = "#F06C64";
+    }
   }, 0)
 };
 
@@ -61,21 +87,17 @@ arrowLeft.addEventListener("click", leftHandler);
 
 //-------------------- Конец слайдера ------------------------------
 
-
-const phoneVertical = document.getElementById("phone-vertical"),
-  phoneHorizontal = document.getElementById("phone-horizontal"),
-  screenVertical = document.getElementById("screen-vertical"),
-  screenHorizontal = document.getElementById("screen-horizontal");
-
-phoneVertical.addEventListener("click", () => {
+const clickVerticalHandler = () => {
   getComputedStyle(screenVertical).display === "none" ? screenVertical.style.display = "block" :
     screenVertical.style.display = "none";
-});
-
-phoneHorizontal.addEventListener("click", () => {
+};
+const clickHorizontalHandler = () => {
   getComputedStyle(screenHorizontal).display === "none" ? screenHorizontal.style.display = "block" :
     screenHorizontal.style.display = "none";
-});
+};
+
+phoneVertical.addEventListener("click", clickVerticalHandler);
+phoneHorizontal.addEventListener("click", clickHorizontalHandler);
 
 //----------------------Конец переключения экранов---------------------------
 
@@ -112,20 +134,22 @@ gallery.addEventListener('click', (event) => {
 
 const contactForm = document.querySelector(".contact-form"),
   contactSend = document.querySelector(".contact-send");
-contactSend.addEventListener("click", (event) => {
+contactForm.addEventListener("submit", (event) => {
   event.preventDefault();
   let subject = document.getElementById('subject'),
     descriptionProject = document.getElementById('description-project'),
     subjectText = '',
     descriptionProjectText = '';
 
-  if (subject.value === "Singolo") {
-    subjectText = "Тема: Singolo"
-  } else if (subject.value === "") subjectText = "Без темы";
+  if (subject.value === "") {
+    subjectText = "Без темы"
+  } else {
+    subjectText = "Тема: " + subject.value
+  }
 
-  if (descriptionProject.value === "Portfolio project") {
-    descriptionProjectText = "Описание: Portfolio project"
-  } else if (descriptionProject.value === "") descriptionProjectText = "Без описания";
+
+  if (descriptionProject.value === "") descriptionProjectText = "Без описания";
+  else descriptionProjectText = "Описание: " + descriptionProject.value;
 
   alert(`Письмо отправлено \n${subjectText} \n${descriptionProjectText}`)
 });
